cb872fdea5ad623a0f2b3f6f8986e3accfb1fc3a
Afuafu <noodle117117@yahoo.co.jp>
2015-03-09 16:48:46 +0900

    Added property

--- lib/user.php
+++ lib/user.php
@@ -2,5 +2,9 @@
 require_once dirname(__FILE__) . '/../functions.php';
 
 class User {
-	
+	private $name = '';	
+	private $email = '';
+	private $password = '';
+	private $created = '';
+	private $modified = '';
 }
