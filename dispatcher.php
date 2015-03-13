2dc4849d73d8ae16dc17e1dcc7db0038113df344
Afuafu <noodle117117@yahoo.co.jp>
2015-03-13 22:17:16 +0900

    Add get_param method

--- dispatcher.php
+++ dispatcher.php
@@ -1,4 +1,6 @@
 <?php
 class Dispatcher {
-	
+	public static function get_param() {
+        return str_replace(dirname($_SERVER['SCRIPT_NAME']), '', substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], '?') ? strpos($_SERVER['REQUEST_URI'], '?') : strlen($_SERVER['REQUEST_URI'])));
+	}
 }
