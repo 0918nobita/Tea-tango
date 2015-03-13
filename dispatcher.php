6a17f0d4578777aed3c22cfb33c3ce4b57d66a42
Afuafu <noodle117117@yahoo.co.jp>
2015-03-13 22:23:55 +0900

    Add dispatch method

--- dispatcher.php
+++ dispatcher.php
@@ -1,5 +1,11 @@
 <?php
 class Dispatcher {
+	public static function dispatch($param, $name, $method = 'GET') {
+		if ($param == self::get_param() && $method == $_SERVER['REQUEST_METHOD']) {
+			require_once $name;
+		}
+	}
+
 	public static function get_param() {
         return str_replace(dirname($_SERVER['SCRIPT_NAME']), '', substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], '?') ? strpos($_SERVER['REQUEST_URI'], '?') : strlen($_SERVER['REQUEST_URI'])));
 	}
