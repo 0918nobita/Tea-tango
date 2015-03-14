1266ae377e6b69d8bd6319da31fdc6644f3995c6
Afuafu <noodle117117@yahoo.co.jp>
2015-03-15 08:40:52 +0900

    Add isLogin method

--- models/login.php
+++ models/login.php
@@ -3,6 +3,10 @@
 class Login {
 	const ME = 'me';
 
+	public static function isLogin() {
+		return isset($_SESSION[self::ME]);
+	}
+
 	public static function getMe($me) {
 		return $_SESSION[self::ME];
 	}
