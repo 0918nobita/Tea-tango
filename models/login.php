e78b0b5b54346b958c57d280f950745a2c64b092
Afuafu <noodle117117@yahoo.co.jp>
2015-03-15 08:40:18 +0900

    Add getMe method

--- models/login.php
+++ models/login.php
@@ -3,6 +3,10 @@
 class Login {
 	const ME = 'me';
 
+	public static function getMe($me) {
+		return $_SESSION[self::ME];
+	}
+
 	public static function setMe($me) {
 		$_SESSION[self::ME] = $me;
 	}
