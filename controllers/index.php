ea6b5acd3cc520d21d836d801364562fdd29e9c9
Afuafu <noodle117117@yahoo.co.jp>
2015-03-15 08:32:10 +0900

    Redirect /login

--- controllers/index.php
+++ controllers/index.php
@@ -2,6 +2,11 @@
 
 require_once __DIR__ . '/../other.php';
 
+if (!isset($_SESSION['me'])) {
+	header('Location: ' . SITE_URL . '/login');
+	exit;
+}
+
 $smarty = getSmartyInstance();
 $smarty->assign('me', User::findById($_SESSION['me']));
 $smarty->display('index.tpl');
