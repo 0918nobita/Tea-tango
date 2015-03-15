23afe1e36ffe26e62212052dac9d46111c230824
Afuafu <noodle117117@yahoo.co.jp>
2015-03-15 09:29:05 +0900

    Add routing to /

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 require_once __DIR__ . '/dispatcher.php';
 
-if (!Dispatcher::get('/login', __DIR__ . '/controllers/login') && 
+if (!Dispatcher::get('/', __DIR__ . '/controllers/index') &&
+	!Dispatcher::get('/login', __DIR__ . '/controllers/login') && 
 	!Dispatcher::post('/login', __DIR__ . '/controllers/login')) {
 	header('HTTP/1.0 404 Not Found');
 	exit;
