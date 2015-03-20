4180418294c2f58fe3c06a82dee597d75bd06103
Afuafu <noodle117117@yahoo.co.jp>
2015-03-20 21:02:21 +0900

    Remove comment

--- language.php
+++ language.php
@@ -1,12 +1,9 @@
 <?php 
-/**
- * Get word
- * 
- * @param string $word word name
- * @param string $lang lang package name
- * 
- * @return string word
- */
+
+function lang($word, $lang = '') {
+	return getWord($word, $lang);
+}
+
 function getWord($word, $lang = '') {
 	if ($lang == '') {
 		$lang = $_SESSION['lang'];
@@ -14,28 +11,12 @@ function getWord($word, $lang = '') {
 	return json_decode(file_get_contents(buildLangFileName($lang)))[$word];
 }
 
-function lang($word, $lang = '') {
-	return getWord($word, $lang);
-}
-
-/**
- * Set lang package
- * 
- * @param string $lang lang package name
- */
 function setLang($lang) {
 	if (is_file(buildLangFileName($lang))) {
 		$_SESSION['lang'] = $lang;
 	}
 }
 
-/**
- * Build lang package file name
- *
- * @param string $lang lang package name
- *
- * @return string lang package file name
- */
 function buildLangFileName($lang) {
 	return 'lang/' . $lang . substr($lang, -1, strlen('.json')) ? '.json' : '';
 }
