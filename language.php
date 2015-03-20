693f142406c666fc1001edd4da185e48728c8157
Afuafu <noodle117117@yahoo.co.jp>
2015-03-20 20:55:26 +0900

    Add buildLangFileName func

--- language.php
+++ language.php
@@ -38,10 +38,21 @@ function lang($a,$b) {
 /**
  * Set lang package
  * 
- * @param string $lang lang name
+ * @param string $lang lang package name
  */
 function setLang($lang) {
 	if (is_file(buildLangFileName($lang))) {
 		$_SESSION['lang'] = $lang;
 	}
+}
+
+/**
+ * Build lang package file name
+ *
+ * @param string $lang lang package name
+ *
+ * @return string lang package file name
+ */
+function buildLangFileName($lang) {
+	return 'lang/' . $lang . substr($lang, -1, strlen('.json')) ? '.json' : '';
 }
