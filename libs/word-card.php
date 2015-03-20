014e77cc4209e48f0289111490eca589b007ca37
Afuafu <noodle117117@yahoo.co.jp>
2015-03-21 07:45:09 +0900

    Add setter

--- libs/word-card.php
+++ libs/word-card.php
@@ -2,8 +2,38 @@
 class WordCard {
 	private $id = 0;
 	private $name = '';
-	private $author_id = 0;
+	private $authorId = 0;
 	private $description = '';
 	private $category = 0;
 	private $created = '';
+
+	public function &setId($id) {
+		$this->id = $id;
+		return $this;
+	}
+
+	public function &setName($name) {
+		$this->name = $name;
+		return $this;
+	}
+
+	public function &setAuthorId($authorId) {
+		$this->authorId = $authorId;
+		return $this;
+	}
+
+	public function &setDescription($description) {
+		$this->description = $description;
+		return $this;
+	}
+
+	public function &setCategory($category) {
+		$this->category = $category;
+		return $this;
+	}
+
+	public function &setCreated($created) {
+		$this->created = $created;
+		return $this;
+	}
 }
