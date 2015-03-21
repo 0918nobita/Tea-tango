904b454afd38dee2700d56385a237ac0d8001deb
Afuafu <noodle117117@yahoo.co.jp>
2015-03-21 20:11:11 +0900

    Fix bug

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/libs/Smarty.class.php';
 
 function &getSmartyInstance() {
 	static $smarty;
-	if (isset($smarty)) {
+	if (!isset($smarty)) {
 		$smarty = new Smarty;
 		$smarty->template_dir = __DIR__ . '/templates';
 		$smarty->compile_dir = __DIR__ . '/templates_c';
