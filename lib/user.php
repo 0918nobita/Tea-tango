6f6d895b7d702996401c0508fc63867abb8f0dc5
Afuafu <noodle117117@yahoo.co.jp>
2015-03-22 09:06:44 +0900

    Add property

--- lib/user.php
+++ lib/user.php
@@ -1,5 +1,54 @@
 <?php
 
 class User {
-	
+	private $id = 0;
+	private $name = '';
+	private $email = '';
+	private $introduce = '';
+	private $created = '';	
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function getEmail() {
+		return $this->email;
+	}
+
+	public function getIntroduce() {
+		return $this->introduce;
+	}
+
+	public function getCreated() {
+		return $this->created;
+	}
+
+	public function &setId($id) {
+		$this->id = $id;
+		return $this;
+	}
+
+	public function &setName($name) {
+		$this->name = $name;
+		return $this;
+	}
+
+	public function &setEmail($email) {
+		$this->email = $email;
+		return $this;
+	}
+
+	public function &setIntroduce($introduce) {
+		$this->introduce = $introduce;
+		return $this;
+	} 
+
+	public function &setCreated($created) {
+		$this->created = $created;
+		return $this;
+	}
 }
