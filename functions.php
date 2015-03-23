b27516479f879c9ae11f6fc3b8a7f98bedac9a45
Afuafu <noodle117117@yahoo.co.jp>
2015-03-23 12:41:03 +0900

    Improve readability

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/config.php';
+require_once __DIR__ . '/config.php';
 
 function connectDb() {
 	static $dbh;
@@ -15,19 +15,19 @@ function connectDb() {
 	return $dbh;
 }
 
-function h($s) {
-	return htmlspecialchars($s,ENT_QUOTES,"UTF-8");
+function h($str) {
+	return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
 
 function setToken() {
-	$token = sha1(uniqid(mt_rand(),true));
+	$token = sha1(uniqid(mt_rand(), true));
 	$_SESSION['token'] = $token;
 }
 
 function checkToken() {
-	if (empty($_SESSION['token'])||($_SESSION['token']!=$_POST['token'])) {
+	if (empty($_SESSION['token']) || ($_SESSION['token'] != $_POST['token'])) {
 		echo '不正なPOSTが行われました。';
-		exit();
+		exit;
 	}
 }
 class User {
@@ -36,29 +36,31 @@ class User {
 	public $introduce;
 	public $created;
 	public $user = array();
+
 	public function findById() {
 		$stmt = connectDb()->query('select * from users where id='.$this->id.' limit 1');
 		while ($data = $stmt->fetch(PDO::FETCH_ASSOC)) {
 			$user[] = $data;
 		}
-		return array('id'=>$user[0]['id'],'name'=>$user[0]['name'],'email'=>$user[0]['email'],'introduce'=>$user[0]['introduce'],'created'=>$user[0]['created']);
+		return array('id' => $user[0]['id'], 'name' => $user[0]['name'], 'email' => $user[0]['email'], 'introduce' => $user[0]['introduce'], 'created' => $user[0]['created']);
 	}
+
 	public function findByName() {
-		$stmt = connectDb()->query('select * from users where name='.$this->name.' limit 1');
+		$stmt = connectDb()->query('select * from users where name=' . $this->name . ' limit 1');
 		while ($data = $stmt->fetch(PDO::FETCH_ASSOC)) {
 			$user[] = $data;
 		}
-		return array('id'=>$user[0]['id'],'name'=>$user[0]['name'],'email'=>$user[0]['email'],'introduce'=>$user[0]['introduce'],'created'=>$user[0]['created']);
+		return array('id' => $user[0]['id'], 'name' => $user[0]['name'], 'email' => $user[0]['email'], 'introduce' => $user[0]['introduce'], 'created' => $user[0]['created']);
 	}
 }
-function emailExists($email,$dbh) {
-	$sql = "select * from users where email = :email limit 1";
+function emailExists($email, $dbh) {
+	$sql = 'select * from users where email = :email limit 1';
 	$stmt = $dbh->prepare($sql);
-	$stmt->execute(array(":email"=>$email));
+	$stmt->execute(array(':email' => $email));
 	$user = $stmt->fetch();
 	return $user ? true : false;
 }
 
-function getSha1Password($s) {
-	return (sha1(PASSWORD_KEY.$s));
+function getSha1Password($str) {
+	return sha1(PASSWORD_KEY . $str);
 }
