e1c10c01bc3c0e4cd100d8bddbe7080740ca7e3c
0918nobita <nobita.0918@gmail.com>
2015-07-27 12:52:14 +0900

    インフォメーション、ビギナーガイドを追加

--- windows/index.php
+++ windows/index.php
@@ -7,7 +7,7 @@
 <style>
 body {
 	font-family: 'Helvetica Neue', Helvetica, 'ヒラギノ角ゴ ProN W3', 'Hiragino Kaku Gothic ProN', メイリオ, Meiryo, sans-serif;
-	padding: 0;
+	padding: 0.5em;
 	margin: 0;
 }
 </style>
@@ -15,5 +15,39 @@ body {
 <body>
 <h1>Tea-tango for Windows</h1>
 <p>Tea-tango for Windowsは、Windows 7/8/8.1をインストールしたPC上で動作するTea-tango公式クライアントアプリです。</p>
+<h2>インフォメーション</h2>
+<ul>
+	<li></li>
+	<li></li>
+</ul>
+<h2>ビギナーガイド</h2>
+<ul>
+	<li><a href="#about">Tea-tangoとは</a></li>
+	<li><a href="#signup">新規ユーザー登録</a></li>
+	<li><a href="#login">ログイン</a></li>
+	<li><a href="#logout">ログアウト</a></li>
+	<li><a href="#editScreenName">スクリーンネーム編集</a></li>
+	<li><a href="#editProfile">自己紹介文編集</a></li>
+	<li><a href="">単語帳投稿</a></li>
+	<li><a href="">単語帳編集</a></li>
+	<li><a href="">単語帳削除</a></li>
+	<li><a href="">単語カード投稿</a></li>
+	<li><a href="">単語カード編集</a></li>
+	<li><a href="">単語カード削除</a></li>
+	<li><a href="">ヘルプ</a></li>
+	<li><a href="">問い合わせ</a></li>
+	<li><a href="">退会</a></li>
+</ul>
+<h3 id="about">Tea-tangoとは</h3>
+<h3 id="signup">新規ユーザー登録</h3>
+<h3 id="login">ログイン</h3>
+<p>「ログイン」ボタンをクリックして、以下の情報を入力してください。</p>
+<ol>
+	<li>ユーザーネームまたはメールアドレス</li>
+	<li>パスワード</li>
+</ol>
+<h3 id="logout">ログアウト</h3>
+<h3 id="editScreenName">スクリーンネーム編集</h3>
+<h3 id="editProfile">自己紹介文編集</h3>
 </body>
 </html>
