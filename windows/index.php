d8418c445bba841b4deef401138f4ea8b060cd3b
0918nobita <nobita.0918@gmail.com>
2015-07-27 09:56:06 +0900

    Tea-tango for Windows用のページを作成

---
+++ windows/index.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>Tea-tango for Windows</title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<style>
+body {
+	font-family: 'Helvetica Neue', Helvetica, 'ヒラギノ角ゴ ProN W3', 'Hiragino Kaku Gothic ProN', メイリオ, Meiryo, sans-serif;
+	padding: 0;
+	margin: 0;
+}
+</style>
+</head>
+<body>
+<h1>Tea-tango for Windows</h1>
+<p>Tea-tango for Windowsは、Windows 7/8/8.1をインストールしたPC上で動作するTea-tango公式クライアントアプリです。</p>
+</body>
+</html>
