bede03fb1a3f8565d66241f9c440957ab32a0ae9
0918nobita <nobita.0918@gmail.com>
2015-07-28 20:59:11 +0900

    fix typo
    相対パスじゃなかったですねココ
    すみません

---
+++ src/templates_c/35868ca8a3592a48080d73f195c83c0618ea923b.file.about.tpl.php
@@ -0,0 +1,44 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-07-28 13:56:39
+         compiled from "C:\xampp\htdocs\Tea-tango\Tea-tango\src\views\about.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:487755b76df7c3dc15-37995495%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '35868ca8a3592a48080d73f195c83c0618ea923b' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\Tea-tango\\Tea-tango\\src\\views\\about.tpl',
+      1 => 1438082248,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '487755b76df7c3dc15-37995495',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_55b76df7c9e2e8_26364146',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_55b76df7c9e2e8_26364146')) {function content_55b76df7c9e2e8_26364146($_smarty_tpl) {?><!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<title>効率的に暗記するならTea-tango！/Tea-tangoとは</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="titleBar">
+	Tea-tangoをはじめよう
+</div>
+<div id="main">
+	<h2>デジタル単語帳を、共有しよう</h2>
+	<p>簡単に単語帳/単語カードを作成することができます。それをタイムラインで公開して、みんなに使ってもらえます。</p>
+	<h2>他のユーザーが作った<br />単語帳/単語カードを<br />「マイライブラリ」に追加しよう</h2>
+	<p>他のユーザーが作成した単語帳/単語カードを、自分の単語帳の置き場所「マイライブラリ」に追加して、使いたいときにすぐ表示できるようにできます。</p>
+	<h2>閲覧だけなら、会員登録は必要ありません</h2>
+	<p></p>
+	<div id="letsstartButton"><a href="index.php?page=card" style="text-decoration: none;">さぁ、はじめよう</a></div>
+</div>
+</body>
+</html><?php }} ?>
