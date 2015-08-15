abf57b15bacd3c8c669501bb16336988fa6445f7
0918nobita <nobita.0918@gmail.com>
2015-08-15 23:56:31 +0900

    OGP設定&Twitterで共有した場合にTwitterカードが表示されるようにmetaタグを追加

--- src/header.php
+++ src/header.php
@@ -4,10 +4,18 @@
 <meta charset="utf-8">
 <meta name="author" content="0918nobita">
 <meta name="description" content="単語帳を作っている時間がもったいない…コンパクトに持ち歩きたい…そんな悩みを解決する、デジタル単語帳共有Webサービスです！">
-<meta property="og:type" content="article">
+<meta property="og:type" content="website">
 <meta property="og:title" content="Tea-tango">
 <meta property="og:description" content="単語帳を作っている時間が省ける！デジタル単語帳共有Webサービス">
 <meta property="og:locale" content="ja_JP">
+<meta property="og:image" content="<?php echo SITE_URL."images/about.jpg"; ?>">
+<!-- Twitter -->
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@0918nobita" />
+<meta name="twitter:title" content="Tea-tango">
+<meta name="twitter:description" content="単語帳を作っている時間が省ける！デジタル単語帳共有Webサービス" />
+<meta name="twitter:image" content="<?php echo SITE_URL."images/about.jpg"; ?>" />
+
 <link rel="stylesheet" href="style.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
