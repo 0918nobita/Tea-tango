2f80b79b60ac45dfa2b2e061ea7f7dfab30efe18
0918nobita <nobita.0918@gmail.com>
2015-08-16 12:14:31 +0900

    TwitterとFB共有の時に表示する画像を追加&favicon.ico追加
    イラレでイラストを作り、icoファイルとpngファイルに変換しました。これが新アイコンになります。

--- src/header.php
+++ src/header.php
@@ -8,14 +8,16 @@
 <meta property="og:title" content="Tea-tango">
 <meta property="og:description" content="単語帳を作っている時間が省ける！デジタル単語帳共有Webサービス">
 <meta property="og:locale" content="ja_JP">
-<meta property="og:image" content="<?php echo SITE_URL."images/about.jpg"; ?>">
+<meta property="og:image" content="http://ttan5.exout.net/images/logo258x258.png">
 <!-- Twitter -->
 <meta name="twitter:card" content="summary" />
 <meta name="twitter:site" content="@0918nobita" />
 <meta name="twitter:title" content="Tea-tango">
 <meta name="twitter:description" content="単語帳を作っている時間が省ける！デジタル単語帳共有Webサービス" />
-<meta name="twitter:image" content="<?php echo SITE_URL."images/about.jpg"; ?>" />
+<meta name="twitter:image" content="http://ttan5.exout.net/images/logo258x258.png" />
 
+<link rel="shortcut icon" href="../images/favicon.ico">
+<link rel="icon" href="../images/favicon.ico">
 <link rel="stylesheet" href="style.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
