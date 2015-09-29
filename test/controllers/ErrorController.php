edc581cf213fa33642f3cd9282bc52658a6ff79c
0918nobita <nobita.0918@gmail.com>
2015-09-29 19:04:29 +0900

    add error page(ErrorController, displayAction, View)
    エラーページのコントローラーと表示アクション、ビューを追加しました。

---
+++ test/controllers/ErrorController.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once __DIR__ . '/../libs/Smarty.class.php';
+
+class ErrorController
+{
+	private $view;
+	private $url;
+
+	public function __construct($url)
+	{
+		$this->view = new Smarty();
+		$this->view->template_dir = __DIR__ . '/../views';
+		$this->view->compile_dir = __DIR__ . '/../templates_c';
+		$this->url = $url;
+		$this->view->assign("url", $this->url);
+	}
+
+	public function displayAction()
+	{
+		$this->view->display('error.tpl');
+	}
+	
+}
---
+++ test/views/error.tpl
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>Tea-tango / ?</title>
+</head>
+<body>
+	{include file="header.tpl"}
+	<h2>エラーが発生しました</h2>
+	<p><a href="">ホーム</a>に戻る</p>
+	{include file="footer.tpl"}
+</body>
+</html>
